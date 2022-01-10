b549cb2eda33b61bd1320c811f0d9af2c9742e9c
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2022-01-10 13:50:12 +0800

    Update tencentddns.lua

--- tencentcloud_ddns/files/luci/controller/tencentddns.lua
+++ tencentcloud_ddns/files/luci/controller/tencentddns.lua
@@ -1,5 +1,5 @@
 module("luci.controller.tencentddns",package.seeall)
 function index()
   
-entry({"admin", "network", "tencentddns"},cbi("tencentddns"),_("TencentDDNS"),20).dependent = true
+entry({"admin", "services", "tencentddns"},cbi("tencentddns"),_("TencentDDNS"),20).dependent = true
 end
